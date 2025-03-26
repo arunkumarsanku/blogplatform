54dbc6689cc470d464ce5e329c8f52b4cc7e092a
arunkumarsanku <arunkumarsanku0408@gmail.com>
2025-03-26 15:05:20 +0100

    implemented FE  routes

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="max-w-md text-center">
+        HI from Quote aaaaas</div>
+    </div>
+  )
+}
