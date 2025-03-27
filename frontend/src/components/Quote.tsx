d361f563734846076dcc2822349481c9aae49638
arunkumarsanku <arunkumarsanku0408@gmail.com>
2025-03-27 09:20:28 +0100

    clear

--- frontend/src/components/Quote.tsx
+++
@@ -1,9 +0,0 @@
-
-export const Quote = () => {
-  return (
-    <div className="bg-slate-200 h-screen flex justify-center flex-col">
-      <div className="max-w-md text-center">
-        HI from Quote aaaaas</div>
-    </div>
-  )
-}
--- frontend/src/pages/Signin.tsx
+++
@@ -1,8 +0,0 @@
-
-export const Signin = () => {
-    return (
-      <div>signin</div>
-    )
-  }
-  
-  
